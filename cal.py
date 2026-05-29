81ca1d55d261c5383841ae4f38996248f09dab87
mariya123-sys <mariyasusan2005@gmail.com>
2026-05-29 19:13:23 +0530

    Created cal.py

---
+++ cal.py
@@ -0,0 +1,22 @@
+num1=float(input("enter first number:"))
+num2=float(input("enter second number:"))
+print("1.Add\n2.Sub\n3.Multiply\4.Divide")
+ch=int(input("enter choice:"))
+if ch==1:
+  print("result:",num1+num2);
+elif ch=2:
+  print("result:",num1-num2);
+elif ch=3:
+  print("result:",num*num2);
+elif ch=4:
+  if (num2==0):
+    print("division not possible");
+  else:
+    print("result:",num1/num2)
+
+  
+
+
+
+  
+  
